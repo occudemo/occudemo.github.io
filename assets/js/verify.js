23169741128e9b650f24005608efddb9c216a3ad
Nathan Pointer <nathanpointer@128-223-223-66-wireless.uoregon.edu>
2015-04-28 17:33:53 -0700

    working

---
+++ assets/js/verify.js
@@ -0,0 +1,29 @@
+UserApp.User.get({}, function(error, result){
+
+	// function getQueryParams(qs) {
+	//     qs = qs.split("+").join(" ");
+	//     var params = {}, tokens,
+	//         re = /[?&]?([^=]+)=([^&]*)/g;
+	//     while (tokens = re.exec(qs)) {
+	//         params[decodeURIComponent(tokens[1])]
+	//             = decodeURIComponent(tokens[2]);
+	//     }
+	//     return params;
+	// }
+
+	// var query = getQueryParams(document.location.search);
+	// if (query.ua_token != undefined){
+	// 	setCookie("ua_session_token", query.ua_token, 1);
+	// 	setCookie("account_type", "manager", 1);
+	// }
+	// 
+	console.log(result);
+	
+	valid = $.inArray( result[0].email, users ) >= 0;
+
+	if(valid){
+		$("#update-skills").show();
+	}else{
+		$("#status").html("you are not on access list, please email alskddkf to request access");
+	}
+});
